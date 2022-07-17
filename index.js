1187a5051940739f1fa69694094b03415e1e320b
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2022-07-17 12:54:09 +0600

    join a room succesfully

--- index.js
+++ index.js
@@ -16,6 +16,11 @@ const io = new Server(expressServer,{
 
 io.on('connection', (socket) => {
   console.log('a user connected');
+ socket.on('join_room',(data)=>{
+    socket.join(data);
+    console.log(`join room id:${socket.id} and room=${data}`)
+ })
+
   socket.on('disconnect',()=>{
      console.log('user disconnected')
   })
