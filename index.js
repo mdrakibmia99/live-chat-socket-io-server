6a7bcebc23fb9c2c8f6dadfbb9b6d9687dc56c97
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2023-07-28 18:11:11 +0600

    test

--- index.js
+++ index.js
@@ -1,37 +1,36 @@
 const express = require('express');
 const app = express();
 const http = require('http');
-const cors=require('cors')
+const cors = require('cors')
 const expressServer = http.createServer(app);
 app.use(cors())
-const Port=process.env.PORT || 5000;
+const Port = process.env.PORT || 5000;
 const { Server } = require("socket.io");
-const io = new Server(expressServer,{
-cors:{
-        origin:"http://localhost:3000/"
-
-    }
+const io = new Server(expressServer, {
+  cors: {
+    origin: "*",
+  }
 
 });
 
 app.get('/', (req, res) => {
-  res.send('<h1>Live Chat server is running</h1>');
+  res.send('<h1>Live Chat server is running....</h1>');
 });
 
 io.on('connection', (socket) => {
   console.log('a user connected');
   // join a room
- socket.on('join_room',(data)=>{
+  socket.on('join_room', (data) => {
     socket.join(data);
     console.log(`join room id:${socket.id} and room=${data}`)
- })
+  })
 
-//   send data form client site 
- socket.on('send_message',(data)=>{
-   socket.to(data.roomId).emit('receive_message',data)
- })
-  socket.on('disconnect',()=>{
-     console.log('user disconnected')
+  //   send data form client site 
+  socket.on('send_message', (data) => {
+    socket.to(data.roomId).emit('receive_message', data)
+  })
+  socket.on('disconnect', () => {
+    console.log('user disconnected')
   })
 });
 
